9171286949772c5525db7a00128fb80c64916611
goplayzig <goplayzig@gmail.com>
2024-11-09 17:00:16 +0900

    Edit init_thread

--- threads/thread.c
+++ threads/thread.c
@@ -476,6 +476,10 @@ init_thread (struct thread *t, const char *name, int priority) {
 	t->init_priority = priority;
 	t->wait_on_lock = NULL;
 	list_init(&(t->donations));
+
+	t->init_priority = priority;
+	t->wait_on_lock = NULL;
+	list_init(&(t->donations));
 }
 
 /* Chooses and returns the next thread to be scheduled.  Should
